c7abdda6b931037a68601696de90a6fe77abadc0
MarkHG <markhg@ciencias.unam.mx>
2018-03-14 14:25:49 -0600

    Busqueda Exponencial

--- practica2.py
+++ practica2.py
@@ -10,23 +10,24 @@ print (busqueda_secuencial([1,2,3,4,5,6,7,8,9,10],9))
 
 def busqueda_binaria (lista, x, a, b):
 
-    centro = int(len(lista)/2)
-    if (a == b + 1):
+    centro = (a+b)//2
+    if (a > b + 1):
         return -1
     elif (lista[centro] == x):
         return centro
     elif (x < lista[centro]):
         return busqueda_binaria(lista,x,a,centro)
-
     elif (x > lista[centro]):
-        return busqueda_binaria(lista,x,b,centro)
+        return busqueda_binaria(lista,x,centro,b)
 
-print (busqueda_binaria([1,2,3,4,5,6,7,8,9,10],1,6,8))
+print (busqueda_binaria([1,2,3,4,5,6,7,8,9,10],2,0,9))
 
 def busqueda_exponencial(lista,n,x):
     if (lista[0] == x):
         return 0
-    limite = 2 ** 1
+    limite = 1
     while (limite < n and lista[limite] <= x):
-        limite = limite * 2
-print (busqueda_exponencial([1,2,3,4,5,6,7,8,9,10],2,3))
+        limite *= 2
+    return busqueda_binaria(lista, x, limite//2,limite)
+
+print (busqueda_exponencial([1,2,3,4,5,6,7,8,9,10],9,1))
